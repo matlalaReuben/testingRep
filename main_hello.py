e10dfee5956fc0a761b748e1c5108634cf07b6e0
matlalaReuben <48084216+matlalaReuben@users.noreply.github.com>
2019-04-06 20:22:03 +0200

    Update main_hello.py

--- main_hello.py
+++ main_hello.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import mysql.connector as mysql
 connection = mysql.connect( user="W39cSeLp8A", database="W39cSeLp8A", host="remotemysql.com", port="3306",password="6b2V9LddHa" )
-mycursor = self.connection.cursor()
+mycursor = connection.cursor()
 print( connection )
 print( mycursor )
 print("hello motherfuckers1")
