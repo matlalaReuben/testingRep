62db21494e0d47e877e51209ad824c0f74840129
matlalaReuben <48084216+matlalaReuben@users.noreply.github.com>
2019-04-06 18:38:20 +0200

    Update main_hello.py

--- main_hello.py
+++ main_hello.py
@@ -1,4 +1,7 @@
 import tkinter as tk
 import mysql.connector as mysql
-
+connection = mysql.connect( user=self.user, database=self.database, host=self.host, port=self.port,password=self.password )
+mycursor = self.connection.cursor()
+print( connection )
+print( mycursor )
 print("hello world")
